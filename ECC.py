f1463393620c3ae4f2dd262e4696803689bdbe81
Hao Hoang <hajuha168@gmail.com>
2021-06-05 07:41:06 +0700

    ECC

--- ECC.py
+++ ECC.py
@@ -3,7 +3,6 @@ import libnum
 import random
 import time
 import hashlib
-import collections
 from libnum import ecc
 
 
@@ -45,6 +44,17 @@ def findModExp(base, e, mod):
     return Y
 
 
+def generate_coprime(phiN):
+    while True:
+        e = random.randint(0, phiN)
+
+        if gcd(e, phiN) != 1:
+            e = random.randint(0, phiN)
+        else:
+            break
+    return e
+
+
 def point_add(curve, point1, point2):
     p, a, b = curve
     if point1 is None:
@@ -59,27 +69,28 @@ def point_add(curve, point1, point2):
         # point1 + (-point1) = 0
         return None
     if x1 == x2:
-        # This is the case point1 == point2.
+        # point1 == point2.
         m = (3 * x1 * x1 + a) * findModInverse(2 * y1, p)
     else:
-        # This is the case point1 != point2.
+        # != point2.
         m = (y1 - y2) * findModInverse(x1 - x2, p)
     x3 = m * m - x1 - x2
     y3 = y1 + m * (x3 - x1)
     result = (x3 % p, -y3 % p)
     return result
 
-
-def scalar_multiply(curve, c, d, point):
+def scalar_multiply(curve, k, point):
+    p, a, b = curve
+    c = ecc.Curve(a, b, p)
     result = None
     addend = point
-    while d:
-        if d & 1:
+    while k:
+        if k & 1:
             # Add.
             result = point_add(curve, result, addend)
         # Double.
         addend = point_add(curve, addend, addend)
-        d >>= 1
+        k >>= 1
     assert c.check(result)
     return result
 
@@ -89,24 +100,77 @@ def generate_elliptic_curve(bitsize):
     p = libnum.generate_prime(bitsize)
 
     while ((4*a**3 + 27 * b ** 2) % p == 0):
-        a = random.randint(0, p-1)
-        b = random.randint(0, p-1)
-    print("Elliptic Curve: y^2 = x^3 + %dx + %db (mod%d)" % (a, b, p))
+        a = generate_coprime(p-1)
+        b = generate_coprime(p-1)
+    print("ELLIPTIC CURVE y^2 = x^3 + %dx + %db (mod%d)" % (a, b, p))
+    print("Some first points on curve:")
+    print(find_points_in_range(1, 20, (p, a, b)))
+    print("Order: ", find_curve_order((p, a, b)))
     return(p, a, b)
+
+
+def find_points_in_range(start, end, curve):
+    p, a, b = curve
+    x = start
+    points = []
+    while True:
+        val = ((x*x*x) + a*x + b) % p
+
+        rtn = libnum.jacobi(val, p)
+
+        if (rtn == 1):
+            res = next(libnum.sqrtmod(val, {p: 1}))
+            points.append((x, int(res)))
+            points.append((x, int(p-res)))
+
+        x = x+1
+        if (x == end or x == p):
+            return points
+
+def find_curve_order(curve):
+    p, a, b = curve
+    x = 1
+    count = 0;
+    while True:
+        val = ((x*x*x) + a*x + b) % p
+        if (val == 0):
+            count = 1
+        rtn = libnum.jacobi(val, p)
+
+        if (rtn == 1):
+            res = next(libnum.sqrtmod(val, {p: 1}))
+            count += 2
+        x = x+1
+
+        if (x == p):
+            return count + 1
+
+def find_order(point, curve):
+    _point = point
+    k = 1
+    i = 1
+
+    while i:
+        k = k + 1
+        if (_point == None):
+            i = 0
+        _point = point_add(curve, point, _point,)
+    return k-1
 
 
 def gerenate_keys(curve):
     p, a, b = curve
 
-    c = ecc.Curve(a, b, p)
-    randX = len(c.find_points_in_range(1, 20))//2
+    randX = len(find_points_in_range(1, 20, curve))//2
 
-    g = c.find_points_in_range(1, 20)[randX]
-    n = c.get_order(g)
-    d = random.randint(0, n)
+    g = find_points_in_range(1, 100, curve)[randX]
+
+    n = find_order(g, curve)
+    d = random.randint(1, n-1)
+    print("\nGENERATE KEY PAIR")
     print("Private key:")
     print("  d = %d" % d)
-    Q = scalar_multiply(curve, c, d, g)
+    Q = scalar_multiply(curve, d, g)
     print("Public key:")
     print("  Elliptic Curve (p, a, b) = (%d, %d, %d)" % (p, a, b))
     print("  Base point g = (%d, %d)" % (g))
@@ -120,7 +184,6 @@ def gerenate_keys(curve):
 
 def sign_message(k, private_key, public_key, message, curve):
     p, a, b = curve
-    _curve = ecc.Curve(a, b, p)
     (g, n, Q) = public_key
     message_hash = hashlib.sha512(message).digest()
     z = int.from_bytes(message_hash, "big")
@@ -129,41 +192,51 @@ def sign_message(k, private_key, public_key, message, curve):
     while not r or not s:
         if k == 0:
             k = random.randrange(1, n - 1)
-        x, _ = scalar_multiply(curve, _curve, k, g)  # _:y
+        x, _ = scalar_multiply(curve, k, g)  # _:y
         r = x % n
         c = findModInverse(k, n)
         s = ((z + r * private_key) * c) % n
-    print("\nSignature:")
-    print("  r = %d" % r)  # 'sigR' in bitcoin transaction.
-    print("  s = %d" % s)
+    print("\nSIGN:")
+
+    print("  Message: %s" % message)  # 'sigR' in bitcoin transaction.
+    print("  Hash massage: %s" % z)
+    print("  r = x1 mod n = %d" % r)  # 'sigR' in bitcoin transaction.
+    print("  s = (h + d*r)* (k ^(- 1)) mod n = %d" % s)
     return (r, s, z)
 
 
 def verify_signature(public_key, signature, curve):
     p, a, b = curve
-    c = ecc.Curve(a, b, p)
     (g, n, Q) = public_key
     r, s, z = signature
+    print("\nVERIFY SIGNATURE:")
+    print("  r = %s" % r)
+    print("  s = %s" % s)
+    print("  Hash massage: %s\n" % z)
     w = findModInverse(s, n)
-    print(w)
     u1 = (z * w) % n
     u2 = (r * w) % n
-    x, _ = point_add(curve, scalar_multiply(curve, c, u1, g),
-                     scalar_multiply(curve, c, u2, Q)
+    print("  u1 = hw mod n = %s" % u1)
+    print("  u2 = rw mod n = %s" % u2)
+    x, y = point_add(curve, scalar_multiply(curve, u1, g),
+                     scalar_multiply(curve, u2, Q)
                      )
+    print("  (x0,y0) = u1g + u2Q = (%d, %d)" % (x, y))
+    print("  r mod n = %s" % (r % n))
+    print("  x1 mod n = %s" % (x % n))
     if (r % n) == (x % n):
         return True  # Signature matches!
     else:
         return False  # Invalid signature!
 
 
-message = b'abcd'
+message = b'Toi yeu Viet Nam'
 time_start = time.time()
 bitsize = int(sys.argv[1])
 curve = generate_elliptic_curve(bitsize)
 private_key, public_key = gerenate_keys(curve)
 signature = sign_message(0, private_key, public_key, message, curve)
-print("\nVerification: %s" %verify_signature(public_key, signature, curve))
+print("\n  Verification: %s" % verify_signature(public_key, signature, curve))
 time_spent = time.time() - time_start
 
 print("Spent time: %.3f sec." % time_spent)
